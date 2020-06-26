e8273b48dba821375ffe940ca4604465a0f13a23
Vijay Srinivasan <cbevijay@gmail.com>
2020-06-26 19:16:34 +0530

    Added Project config files to source repository and added comments to code

--- HelloMLWorld.py
+++ HelloMLWorld.py
@@ -6,14 +6,18 @@ import numpy as np
 
 def build_execute_model(epochs_value, input_for_prediction):
 
+    # Define the model
     model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
     model.compile(optimizer='sgd', loss='mean_squared_error')
 
+    # Samples and Labels
     xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
     ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
+    # Model training
     model.fit(xs, ys, epochs=epochs_value)
 
+    # Predict using model
     print(model.predict([input_for_prediction]))
 
 
